2fc9f054f5921540f79863b4fa7c84840d8ba27f
Denis Velasquez <vgdenisomar@gmail.com>
2019-06-07 11:39:07 -0600

    Get por id agregado, uuid agregado

--- routes/api/clases.js
+++ routes/api/clases.js
@@ -1,6 +1,8 @@
 const express= require('express');
 var router = express.Router();
 
+var uuid = require('uuid/v4');
+
 var clasesCollection=[];
 
 var clasesStruct={
@@ -16,7 +18,7 @@ clasesCollection.push(
     Object.assign(
         clasesStruct,
         {
-            "id":"uu1",
+            "id":uuid(),
             "clase":"Seminario de Software",
             "seccion":"1801",
             "cupos":"30",
@@ -31,8 +33,22 @@ router.get('/',(req,res,next)=>{
     res.status(200).json(clasesCollection);
 });
 
+router.get('/:id', (req, res, next) => {
+    if  (!req.params.id) return next();
+    var id = req.params.id;
+    var clases = clasesCollection.filter((e, i) => {
+      return (e.id === id);
+    });
+  
+    if (clases.length > 0) {
+      res.status(200).json(clases[0]);
+    } else {
+      res.status(404).json({});
+  }
+});
+
 router.post('/',(req,res,next)=>{
-    var newElement=Object.assign({},clasesStruct,req.body);
+    var newElement=Object.assign({},clasesStruct,{id:uuid()},req.body);
     clasesCollection.push(newElement);
     res.status(200).json(clasesCollection);
 });
